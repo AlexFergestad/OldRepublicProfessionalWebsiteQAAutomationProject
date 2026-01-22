4d9ade46c60ed54bb03874967ca7918e9ce46d0f
Alex Fergestad <ajfergestad@comcast.net>
2026-01-21 20:46:27 -0600

    Added to test_homepage.py

--- tests/ui/pages/test_homepage.py
+++ tests/ui/pages/test_homepage.py
@@ -80,7 +80,7 @@ def test_insurance_type_cards_and_links_visible(page: Page, base_url):
         # Finds the card image element
 
 
-    # Double check the correct elements of the titles and then do the images are visible for this test case - done
+    # Double checks the correct elements of the titles and then do the images are visible for this test case - done
 
 # Tests that I want done:
 # - Contact and careers in separate files
