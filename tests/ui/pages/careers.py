3b6e1fed3ee90abae4398bec27dd5e58e71c6b03
Alex Fergestad <ajfergestad@comcast.net>
2026-02-08 15:35:44 -0600

    Added to careers.py

---
+++ tests/ui/pages/careers.py
@@ -0,0 +1,11 @@
+"""
+
+Careers UI Tests
+Test Cases: TC-
+
+* This page verifies the career page of the Old Republic Professional website.
+
+"""
+
+# Maybe try to add a fixture that marks these tests as career page tests, 
+# so we can easily run them separately if needed.
