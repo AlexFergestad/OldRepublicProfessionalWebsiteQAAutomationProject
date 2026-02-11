31a3d0da326ab47576d7f5ef4de9cec0bed9e297
Alex Fergestad <ajfergestad@comcast.net>
2026-02-10 20:28:41 -0600

    Added to test_careers.py

--- tests/ui/pages/test_careers.py
+++ tests/ui/pages/test_careers.py
@@ -13,37 +13,35 @@ from playwright.sync_api import Page, expect
 # Maybe try to add a fixture that marks these tests as career page tests, 
 # so we can easily run them separately if needed.
 
-"""Verify that the careers page loads correctly and has the correct URL when accessed from the home page."""
-@pytest.mark.ui
-@pytest.mark.careers
-def test_careers_page_loads(page: Page, base_url):
+# """Verify that the careers page loads correctly and has the correct URL when accessed from the home page."""
+# @pytest.mark.ui
+# @pytest.mark.careers
+# def test_careers_page_loads(page: Page, base_url):
 
-    # Goes to the home page first
-    page.goto(base_url)
+#     # Goes to the home page first
+#     page.goto(base_url)
     
-    # Clicks on the Careers menu item to navigate to the careers page
-    page.get_by_role("menuitem", name="Careers").click()
+#     # Clicks on the Careers menu item to navigate to the careers page
+#     page.get_by_role("menuitem", name="Careers").click()
 
-    # Verifies that the URL is correct for the careers page
-    expect(page).to_have_url(f"{base_url}/careers")
+#     # Verifies that the URL is correct for the careers page
+#     expect(page).to_have_url(f"{base_url}/careers")
 
-"""Verify that the browser tab name is correct when on the careers page."""
-@pytest.mark.ui
-@pytest.mark.careers
-def test_careers_page_browser_tab(page: Page, base_url):
+# """Verify that the browser tab name is correct when on the careers page."""
+# @pytest.mark.ui
+# @pytest.mark.careers
+# def test_careers_page_browser_tab(page: Page, base_url):
 
-    # Goes to the careers page directly
-    page.goto(f"{base_url}/careers")
-    page.wait_for_load_state("networkidle")
+#     # Goes to the careers page directly
+#     page.goto(f"{base_url}/careers")
+#     page.wait_for_load_state("networkidle")
 
-    # Verifies that the browser tab name is correct
-    expect(page).to_have_title("Careers at ORPRO")
-
-    # Have the page wait for 4 seconds
-    page.wait_for_timeout(4000)
+#     # Verifies that the browser tab name is correct
+#     expect(page).to_have_title("Careers at ORPRO")
 
 
-# Test the browser tab name 
+
+# Test the browser tab name --
 # Test the h1 headin "Careers"
 # Test the LinkedIn link
 # Test the intern job listing and maybe more inside of it
