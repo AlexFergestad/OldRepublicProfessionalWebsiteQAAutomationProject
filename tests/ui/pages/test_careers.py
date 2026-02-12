cb9e96d603ea318477faec6ff24f32277edd3d4e
Alex Fergestad <ajfergestad@comcast.net>
2026-02-11 22:29:44 -0600

    Added to test_careers.py

--- tests/ui/pages/test_careers.py
+++ tests/ui/pages/test_careers.py
@@ -39,7 +39,7 @@ from playwright.sync_api import Page, expect
 #     # Verifies that the browser tab name is correct
 #     expect(page).to_have_title("Careers at ORPRO")
 
-
+"""Verify the Careers Header is on the Careers page."""
 
 # Test the browser tab name --
 # Test the h1 headin "Careers"
