5c72993dad7701c9f94919b4432fd1cabfa3892c
Alex Fergestad <ajfergestad@comcast.net>
2026-02-26 23:15:25 -0600

    Added to test_careers.py file

--- tests/ui/test_careers.py
+++ tests/ui/test_careers.py
@@ -173,7 +173,7 @@ from tests.ui.page_objects.careers_job_details_page_object import JobDetailsPage
 """TC-009: Verify Culture Section"""
 @pytest.mark.ui
 @pytest.mark.careers
-def 
+def test_culture_section
 
 # Test the browser tab name --
 # Test the h1 headin "Careers"
