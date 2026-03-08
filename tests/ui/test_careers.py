5d51e8beceae26bd920afa022ba3169cee45f60a
Alex Fergestad <ajfergestad@comcast.net>
2026-03-08 13:47:21 -0500

    Added to careers_job_details_page_object.py

--- tests/ui/test_careers.py
+++ tests/ui/test_careers.py
@@ -206,6 +206,7 @@ from tests.ui.page_objects.careers_job_details_page_object import JobDetailsPage
 
 """TC-011: Verify Email Popup "Submit Your Resume" Button"""
 @pytest.mark.ui
+@pytest.mark.careers
 
 # Test the browser tab name --
 # Test the h1 headin "Careers"
