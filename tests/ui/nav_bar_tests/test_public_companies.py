ea09d679e067da2d755f0f246e3f831ed34ff5c3
Alex Fergestad <ajfergestad@comcast.net>
2026-03-11 21:00:33 -0500

    Added to test_public_companies.py file

--- tests/ui/nav_bar_tests/test_public_companies.py
+++ tests/ui/nav_bar_tests/test_public_companies.py
@@ -1,1 +1,13 @@
 # This file is for automating the testin of the public companies pages in the nav bar.
+
+"""
+
+Private Companies Nav Bar Section UI Tests
+Test Cases: TC-001, TC-002, TC-003, TC-004, TC-005, TC-006, TC-007, TC-008, TC-009, TC-010, TC-011
+
+* This page verifies the homepage of the Old Republic Professional website loads correctly, 
+has the correct title and main header, displays the company description, shows all insurance type cards 
+with correct titles and links, has a working Contact Us link, displays the company logo that links to the 
+homepage, has a functional search bar, and includes the expected links above the footer.
+
+"""
