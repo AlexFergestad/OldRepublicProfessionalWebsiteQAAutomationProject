f8b97a2eb68d899f850cbf527ef41bef0218de39
Alex Fergestad <ajfergestad@comcast.net>
2026-03-12 19:10:43 -0500

    Added nav_bar_page_objects.py file

--- tests/ui/page_objects/nav_bar_page_objects.py
+++ tests/ui/page_objects/nav_bar_page_objects.py
@@ -2,3 +2,9 @@
 
 from playwright.sync_api import Page, expect
 
+
+class NavigationMenu:
+    """Page Object for the main navigation menu"""
+    
+    def __init__(self, page: Page):
+        self.page = page
