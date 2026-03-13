d5dd3c48b2947e24e8a97b7aea2a5aa74851ac7b
Alex Fergestad <ajfergestad@comcast.net>
2026-03-12 19:16:46 -0500

    Added nav bar page objects

--- tests/ui/page_objects/nav_bar_page_objects.py
+++ tests/ui/page_objects/nav_bar_page_objects.py
@@ -8,3 +8,10 @@ class NavigationMenu:
     
     def __init__(self, page: Page):
         self.page = page
+
+        # Main navigation item locators
+        self.public_companies_link = page.get_by_role("menuitem", name="Public Companies").first
+        self.private_companies_link = page.get_by_role("menuitem", name="Private Companies").first
+        self.law_firms_link = page.get_by_role("menuitem", name="Law Firms").first
+        self.financial_institutions_link = page.get_by_role("menuitem", name="Financial Institutions").first
+        self.commercial_crime_link = page.get_by_role("menuitem", name="Commercial Crime").first
