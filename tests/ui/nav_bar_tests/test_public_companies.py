998b65e43ddb5d5b2ce2f274370b04acfdbdbb22
Alex Fergestad <ajfergestad@comcast.net>
2026-03-15 16:09:30 -0500

    Added to test_public_companies.py

--- tests/ui/nav_bar_tests/test_public_companies.py
+++ tests/ui/nav_bar_tests/test_public_companies.py
@@ -39,8 +39,8 @@ def test_public_companies_page_loads(page: Page, base_url):
     page.wait_for_load_state("networkidle")
 
 # """TC-002: Verify Browser Tab Name on Public Companies Page"""
-@pytest.mark.ui
-@pytest.mark.careers
+# @pytest.mark.ui
+# @pytest.mark.careers
 
 
 # """TC-003: Verify Public-Company Liability Overview Heading and Paragraph"""
