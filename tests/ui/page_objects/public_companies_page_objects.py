1a79dcf82a90767eb4d9727400c8b4a633e1446d
Alex Fergestad <ajfergestad@comcast.net>
2026-03-20 19:33:40 -0500

    Added to public_comapanies_page_objects.py

--- tests/ui/page_objects/public_companies_page_objects.py
+++ tests/ui/page_objects/public_companies_page_objects.py
@@ -1,7 +1,11 @@
 from playwright.sync_api import Page, expect
+from pytest_playwright.pytest_playwright import page
 
 class Public_Company_Liability_Overview:
     def __init__(self, page: Page):
         self.page = page
 
-    # Locators
+        # Locators
+        self.page_heading = page.locator("h1").first
+    
+    def 
