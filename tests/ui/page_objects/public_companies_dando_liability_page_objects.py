64e359669fd806e8595dc3d0ba68c16a97e6ef9c
Alex Fergestad <ajfergestad@comcast.net>
2026-04-13 21:04:12 -0500

    Added to public_companies_dando_liability_page_objects.py

---
+++ tests/ui/page_objects/public_companies_dando_liability_page_objects.py
@@ -0,0 +1,8 @@
+
+from playwright.sync_api import Page, expect
+
+# Page Objects - relative import from same ui folder
+from tests.ui.page_objects.careers_page_object import CareersPage
+from tests.ui.page_objects.nav_bar_page_objects import NavigationMenu
+from tests.ui.page_objects.public_companies_page_objects import Public_Company_Liability_Overview
+
