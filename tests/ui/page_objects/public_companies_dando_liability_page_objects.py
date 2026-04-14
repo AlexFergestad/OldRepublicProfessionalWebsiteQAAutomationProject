897feb55f0a4f412a901908d1140eec17aca0d16
Alex Fergestad <ajfergestad@comcast.net>
2026-04-13 21:05:22 -0500

    Added to public_companies_dando_liability_page_objects.py

--- tests/ui/page_objects/public_companies_dando_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_dando_liability_page_objects.py
@@ -6,3 +6,8 @@ from tests.ui.page_objects.careers_page_object import CareersPage
 from tests.ui.page_objects.nav_bar_page_objects import NavigationMenu
 from tests.ui.page_objects.public_companies_page_objects import Public_Company_Liability_Overview
 
+class Public_Company_Dando_Liability_Page:
+    def __init__(self):
+        # Locators for the Directors and Officers Liability page
+        self.page_title_locator = "h1"
+        self.page_url = "/public-companies/directors-and-officers-liability"
