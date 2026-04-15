e2435ed6f5a782ac21d0a787669855c0eea537af
Alex Fergestad <ajfergestad@comcast.net>
2026-04-14 21:53:44 -0500

    Added to public_companies_dando_liability_page_objects.py

--- tests/ui/page_objects/public_companies_dando_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_dando_liability_page_objects.py
@@ -1,6 +1,8 @@
 
 from playwright.sync_api import Page, expect
 
+from conftest import page
+
 # Page Objects - relative import from same ui folder
 from tests.ui.page_objects.careers_page_object import CareersPage
 from tests.ui.page_objects.nav_bar_page_objects import NavigationMenu
@@ -11,6 +13,7 @@ class Public_Company_Dando_Liability_Page:
         # Locators for the Directors and Officers Liability page
         self.page_title_locator = "h1"
         self.page_url = "/public-companies/directors-and-officers-liability"
+        self.dAndOPage = page.locatorpage.locator("a[href='/business-insurance-public-company/directors-and-officers-liability'][role='menuitem']")
     
     def navigate_to_directors_and_officers_liability_page(self, page: Page):
 
