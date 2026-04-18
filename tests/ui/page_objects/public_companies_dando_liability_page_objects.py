51c0d73193fe2337c8b5ef0c7d8bd42462fdcfee
Alex Fergestad <ajfergestad@comcast.net>
2026-04-18 16:22:26 -0500

    Added to public_companies_dando_liability_page_objects.py

--- tests/ui/page_objects/public_companies_dando_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_dando_liability_page_objects.py
@@ -1,23 +1,17 @@
-
 from playwright.sync_api import Page, expect
 
-from conftest import page
-
-# Page Objects - relative import from same ui folder
 from tests.ui.page_objects.careers_page_object import CareersPage
 from tests.ui.page_objects.nav_bar_page_objects import NavigationMenu
 from tests.ui.page_objects.public_companies_page_objects import Public_Company_Liability_Overview
 
 class Public_Company_Dando_Liability:
-    def __init__(self, base_url: str):
-        # Locators for the Directors and Officers Liability page
+    def __init__(self, page: Page):
         self.page = page
 
-        self.page_title_locator = "h1"
-        self.page_url = "/public-companies/directors-and-officers-liability"
+        self.page_title_locator = page.locator("h1")
+        self.page_url = "/business-insurance-public-company/directors-and-officers-liability"
         self.dAndOPage = page.get_by_role("menuitem", name="Directors and Officers Liability")
     
-    def navigate_to_directors_and_officers_liability_page(self, page: Page):
-        # Clicks on the Directors and Officers Liability card to navigate to the directors and officers liability page
+    def navigate_to_directors_and_officers_liability_page(self):
         self.dAndOPage.click()
-        page.wait_for_load_state("networkidle")
+        self.page.wait_for_load_state("networkidle")
