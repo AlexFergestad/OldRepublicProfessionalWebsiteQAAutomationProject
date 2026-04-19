829f024317809c05d9f5485a8c0a3a88fe3c1ad0
Alex Fergestad <ajfergestad@comcast.net>
2026-04-19 18:22:15 -0500

    Added to test_public_companies_dando.py

--- tests/ui/nav_bar_tests/test_public_companies_dando.py
+++ tests/ui/nav_bar_tests/test_public_companies_dando.py
@@ -11,18 +11,18 @@ from tests.ui.page_objects.public_companies_page_objects import Public_Company_L
 from tests.ui.page_objects.public_companies_dando_liability_page_objects import Public_Company_Dando_Liability
 
 
-"""TC-01: Verify that the public companies directors and officers liability page loads correctly and has the correct URL when accessed from the home page."""
-@pytest.mark.ui
-@pytest.mark.public_companies_directors_and_officers_liability_page
-def test_public_companies_directors_and_officers_liability_page_loads(page: Page, base_url):
-    # Goes to the home page first
-    page.goto(base_url)
+# """TC-01: Verify that the public companies directors and officers liability page loads correctly and has the correct URL when accessed from the home page."""
+# @pytest.mark.ui
+# @pytest.mark.public_companies_directors_and_officers_liability_page
+# def test_public_companies_directors_and_officers_liability_page_loads(page: Page, base_url):
+#     # Goes to the home page first
+#     page.goto(base_url)
 
-    # Clicks on the Public Companies menu item to navigate to the public companies page
-    NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
+#     # Clicks on the Public Companies menu item to navigate to the public companies page
+#     NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
     
-    # Clicks on the Directors and Officers Liability card to navigate to the directors and officers liability page
-    Public_Company_Dando_Liability(page).navigate_to_directors_and_officers_liability_page(page)
+#     # Clicks on the Directors and Officers Liability card to navigate to the directors and officers liability page
+#     Public_Company_Dando_Liability(page).navigate_to_directors_and_officers_liability_page()
 
-    # Waits for the directors and officers liability page to load
-    page.wait_for_load_state("networkidle")
+#     # Waits for the directors and officers liability page to load
+#     page.wait_for_load_state("networkidle")
