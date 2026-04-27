3fb4627d9fc405c2b28080cf5ed5ce990a17018f
Alex Fergestad <ajfergestad@comcast.net>
2026-04-27 13:07:20 -0500

    Added to test_cases_public_companies_dando.md

--- tests/ui/page_objects/public_companies_dando_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_dando_liability_page_objects.py
@@ -20,6 +20,5 @@ class Public_Company_Dando_Liability:
     def verify_title(self):
         expect(self.page_title_locator).to_have_text("Directors and Officers Liability Insurance")
 
-    def verify_who_we_are(page):
-        
-        
+    def verify_who_we_are(self):
+        expect(self.who_we_are_paragraph).to_be_visible()
