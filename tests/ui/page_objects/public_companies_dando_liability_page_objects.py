c3f05614aa10ec9b9e750e9882e88aa6a552b475
Alex Fergestad <ajfergestad@comcast.net>
2026-04-27 20:33:12 -0500

    Added to public_companies_dando_liability_page_objects.py

--- tests/ui/page_objects/public_companies_dando_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_dando_liability_page_objects.py
@@ -12,6 +12,7 @@ class Public_Company_Dando_Liability:
         self.page_url = "/business-insurance-public-company/directors-and-officers-liability"
         self.dAndOPage = page.locator("#hs_menu_wrapper_module_1527184808535133_mjfm_header_main_menu").get_by_role("menuitem", name="Directors and Officers Liability")
         self.who_we_are_paragraph = page.locator("p").filter(has_text="established in December 1983")
+        self.who_we_are = page.locator("h2")
     
     def navigate_to_directors_and_officers_liability_page(self):
         self.dAndOPage.click()
@@ -21,4 +22,5 @@ class Public_Company_Dando_Liability:
         expect(self.page_title_locator).to_have_text("Directors & Officers Liability")
 
     def verify_who_we_are(self):
+        expect(self.who_we_are).to_have_text("Who We Are:")
         expect(self.who_we_are_paragraph).to_be_visible()
