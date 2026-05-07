1e782fa83819af611d2bec501c66c6e9da3f1805
Alex Fergestad <ajfergestad@comcast.net>
2026-05-06 21:14:47 -0500

    Added to test_public_companies_dando.py

--- tests/ui/nav_bar_tests/test_public_companies_dando.py
+++ tests/ui/nav_bar_tests/test_public_companies_dando.py
@@ -60,30 +60,30 @@ from tests.ui.page_objects.public_companies_dando_liability_page_objects import 
 #     # Clicks the D&O link from the Public Companies overview page object
 #     public_companies_page = Public_Company_Liability_Overview(page, base_url)
 #     public_companies_page.go_to_subpage(public_companies_page.directors_officers_link)
-    
+
 #     # Verifies the What We Offer Paragraph
 #     Public_Company_Dando_Liability(page).verify_what_we_offer()
 
-"""TC-04: Verify Public Companies Section on the Right"""
-@pytest.mark.ui
-@pytest.mark.public_companies_first_page
-def test_public_companies_right_section(page: Page, base_url):
-    """Verify Public Companies Section on the Right"""
+# """TC-04: Verify Public Companies Section on the Right"""
+# @pytest.mark.ui
+# @pytest.mark.public_companies_first_page
+# def test_public_companies_right_section(page: Page, base_url):
+#     """Verify Public Companies Section on the Right"""
         
-    # Navigate to the home page
-    page.goto(base_url)
+#     # Navigate to the home page
+#     page.goto(base_url)
 
-    # Click on the Public Companies menu item to navigate to the public companies page
-    NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
+#     # Click on the Public Companies menu item to navigate to the public companies page
+#     NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
 
-    # Wait for the public companies page to load
-    page.wait_for_load_state("networkidle")
+#     # Wait for the public companies page to load
+#     page.wait_for_load_state("networkidle")
 
-    # Verify the right section of the public companies page
-    public_companies = Public_Company_Liability_Overview(page, base_url)
+#     # Verify the right section of the public companies page
+#     public_companies = Public_Company_Liability_Overview(page, base_url)
 
-    # Verify the links in the right section go to the correct pages and have the correct titles
-    public_companies.verify_public_companies_right_section()
+#     # Verify the links in the right section go to the correct pages and have the correct titles
+#     public_companies.verify_public_companies_right_section()
     
 
 # Finished TC-04, do md file for it then verify performance and accessibility for the directors and officers public companies page.
