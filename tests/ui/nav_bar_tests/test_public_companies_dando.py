2036508e48121a8719335efb5ec4cccdf23e32e8
Alex Fergestad <ajfergestad@comcast.net>
2026-05-07 20:28:53 -0500

    Added to test_public_companies_dando.py

--- tests/ui/nav_bar_tests/test_public_companies_dando.py
+++ tests/ui/nav_bar_tests/test_public_companies_dando.py
@@ -89,5 +89,17 @@ from tests.ui.page_objects.public_companies_dando_liability_page_objects import 
 @pytest.mark.ui
 @pytest.mark.public_companies_directors_and_officers_liability_page
 def test_verify_dando_products_link(page: Page, base_url):
+    # Goes to the home page
+    page.goto(base_url)
+
+    # Clicks on the Public Companies menu item to navigate to the public companies page
+    NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
+
+    # Clicks the D&O link from the Public Companies overview page object
+    public_companies_page = Public_Company_Liability_Overview(page, base_url)
+    public_companies_page.go_to_subpage(public_companies_page.directors_officers_link)
+
+    # Verifies the D&O Products link in the right section goes to the correct page and has the correct title
+    Public_Company_Dando_Liability(page).verify_dando_products_link()
 
 # Finished TC-04, do md file for it then verify performance and accessibility for the directors and officers public companies page.
