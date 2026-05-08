7c8fc15104085653b52b460d2ba37c5f3d4fca79
Alex Fergestad <ajfergestad@comcast.net>
2026-05-07 20:18:16 -0500

    Added to test_cases_public_companies.md

--- tests/ui/nav_bar_tests/test_public_companies_dando.py
+++ tests/ui/nav_bar_tests/test_public_companies_dando.py
@@ -85,6 +85,6 @@ from tests.ui.page_objects.public_companies_dando_liability_page_objects import 
 #     # Verify the links in the right section go to the correct pages and have the correct titles
 #     public_companies.verify_public_companies_right_section()
 
-"""TC-05:"""
+"""TC-05: Verify D&O Products Link"""
 
 # Finished TC-04, do md file for it then verify performance and accessibility for the directors and officers public companies page.
