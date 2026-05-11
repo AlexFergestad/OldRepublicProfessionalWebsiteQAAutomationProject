26bc02f411aff400e5cacb2cef21351154cf9945
Alex Fergestad <ajfergestad@comcast.net>
2026-05-10 22:49:55 -0500

    Added to test_public_companies_dando.py

--- tests/ui/nav_bar_tests/test_public_companies_dando.py
+++ tests/ui/nav_bar_tests/test_public_companies_dando.py
@@ -104,6 +104,7 @@ from tests.ui.page_objects.public_companies_dando_liability_page_objects import 
 
 """TC-06: Verify Performance of Directors and Officers Liability Page"""
 @pytest.mark.ui
+@pytest.mark.public_companies_directors_and_officers_liability_page
 
 
 
