e2934de2a91b30a353db87358f63c8068b318164
Alex Fergestad <ajfergestad@comcast.net>
2026-05-23 12:59:41 -0500

    Added to test_public_companies_dando.py

--- tests/ui/nav_bar_tests/test_public_companies_dando.py
+++ tests/ui/nav_bar_tests/test_public_companies_dando.py
@@ -15,10 +15,8 @@ from tests.ui.page_objects.public_companies_dando_liability_page_objects import 
 Public Companies Directors and Liability Page UI Tests
 Test Cases: TC-001, TC-002, TC-003, TC-004, TC-005, TC-006, TC-007
 
-* This page verifies the homepage of the Old Republic Professional website loads correctly, 
-has the correct title and main header, displays the company description, shows all insurance type cards 
-with correct titles and links, has a working Contact Us link, displays the company logo that links to the 
-homepage, has a functional search bar, and includes the expected links above the footer.
+* This page verifies the Directors and Liability page of the Old Republic Professional website loads correctly, 
+has the correct title and headers, performanced checks the page, and accessibility checks the page.
 
 """
 
