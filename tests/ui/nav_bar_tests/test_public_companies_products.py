adcf4835344e811106a9ae4699c1ab46ded03490
Alex Fergestad <ajfergestad@comcast.net>
2026-05-24 13:02:19 -0500

    Added to test_ public_companies_products file

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -23,3 +23,4 @@ has the correct title and headers, performanced checks the page, and accessibili
 """TC-01: Verify that the public companies products page loads correctly and has the correct URL when accessed from the home page."""
 @pytest.mark.ui
 @pytest.mark.public_companies_products_page
+def test_public_companies_products_page_loads(page: Page, base_url):
