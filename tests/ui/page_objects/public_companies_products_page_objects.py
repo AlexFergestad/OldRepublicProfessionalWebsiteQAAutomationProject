69bf4756da463206dbc1b5f09b1cb03943630f83
Alex <ajfergestad@comcast.net>
2026-05-28 20:29:23 -0500

    Added to public_companies_products_page_objects.py

--- tests/ui/page_objects/public_companies_products_page_objects.py
+++ tests/ui/page_objects/public_companies_products_page_objects.py
@@ -6,6 +6,7 @@ class Public_Company_Products:
     def __init__(self, page: Page, base_url: str):
         self.page = page
         self.base_url = base_url
+        self.productsPage = page.locator() 
         self.url = f"{base_url}/business-insurance-public-company/directors-and-officers-liability/products"
 
     def navigate_to_products_page(self):
