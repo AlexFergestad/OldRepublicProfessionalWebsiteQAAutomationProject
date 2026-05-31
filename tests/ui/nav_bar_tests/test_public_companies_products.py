9aacdf7db77ce0d82b5fca542b833c4f5674f313
Alex <ajfergestad@comcast.net>
2026-05-31 13:40:33 -0500

    Added to test_public_companies_products.py

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -48,3 +48,5 @@ def test_h1_title_in_products_page(page: Page, base_url):
 
     # Clicks on the Products link to navigate to the products page
     Public_Company_Products(page, base_url).navigate_to_products_page()
+
+    # Verifies that the H1 title is correct
