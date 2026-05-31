02bdaf4ca1940a8de5f980c31def60c3ef01c3c5
Alex <ajfergestad@comcast.net>
2026-05-31 13:30:05 -0500

    Added to test_public_companies_products.py

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -45,3 +45,5 @@ def test_h1_title_in_products_page(page: Page, base_url):
 
     # Clicks on the Products link to navigate to the products page
     Public_Company_Products(page, base_url).navigate_to_products_page()
+
+    # Clicks on the Products link to navigate to the products page
