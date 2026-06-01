49b9500152e87e1c5da838bc8634aeadb5dedc2d
Alex <ajfergestad@comcast.net>
2026-06-01 16:54:46 -0500

    Added to public_companies_products_page_objects.py

--- tests/ui/page_objects/public_companies_products_page_objects.py
+++ tests/ui/page_objects/public_companies_products_page_objects.py
@@ -16,3 +16,4 @@ class Public_Company_Products:
 
     def verify_products_h1(self):
         expect(self.products_h1).to_have_text("Public Company Directors & Officers Liability Underwriting Products")
+        expect(self.products_h1).to_be_visible()
