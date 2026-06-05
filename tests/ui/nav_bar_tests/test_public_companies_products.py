b15ef6976cb2eac871f107980ae3371ada1b5db3
Alex <ajfergestad@comcast.net>
2026-06-04 19:19:47 -0500

    Added to test public companies products page

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -65,4 +65,4 @@ def test_primary_traditional_d_and_o_bullet_point(page: Page, base_url):
     # Clicks on the Products link to navigate to the products page
     Public_Company_Products(page, base_url).navigate_to_products_page()
 
-    # Clicks on the 
+    # Clicks on the Primary,
