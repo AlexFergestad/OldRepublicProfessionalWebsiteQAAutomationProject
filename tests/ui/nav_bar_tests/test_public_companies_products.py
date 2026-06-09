3158e5212008bafb0a2f3a5602d1535c0fd0622b
Alex <ajfergestad@comcast.net>
2026-06-09 18:54:43 -0500

    Added to the products test page

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -66,6 +66,6 @@ def test_primary_traditional_d_and_o_bullet_point(page: Page, base_url):
     Public_Company_Products(page, base_url).navigate_to_products_page()
 
     # Clicks on the Primary, Traditional D&O bullet point to verify it is clickable and navigates to the correct page
-
+    Public_Company_Products(page, base_url).verify_primary_traditional_d_and_o_bullet_point()
 
 """TC-04: Verify """
