752b5ec6e5f1f74c1e84b28cecf76ecefcd64eaf
Alex Fergestad <ajfergestad@comcast.net>
2026-06-17 22:49:07 -0500

    Added to public companies products page objects

--- tests/ui/page_objects/public_companies_products_page_objects.py
+++ tests/ui/page_objects/public_companies_products_page_objects.py
@@ -29,3 +29,4 @@ class Public_Company_Products:
         expect(primary_do_links.get_by_role("link", name="Policy form ORUG-95")).to_be_visible()
     
     def verify_excess_liability_bullet_point():
+        expect
