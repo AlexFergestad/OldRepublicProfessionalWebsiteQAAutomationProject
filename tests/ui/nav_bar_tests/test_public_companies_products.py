525a517585a59de80e82cba04881ac297968ddbb
Alex Fergestad <ajfergestad@comcast.net>
2026-06-26 17:59:51 -0500

    Added to test public companies products file

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -111,4 +111,5 @@ def test_excess_side_a_bullet_point(page: Page, base_url):
     # Clicks on the Products link to navigate to the public companies nav bar dropdown
     NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
 
-    
+    # Clicks on the Products link to navigate to the products page
+    Public_Company_Products(page, base_url).navigate_to_products_page()
