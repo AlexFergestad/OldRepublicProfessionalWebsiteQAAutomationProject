3d1967267862fe686fec3b3f099399af3acce117
Alex Fergestad <ajfergestad@comcast.net>
2026-06-26 17:59:11 -0500

    Added to test public companies products file

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -105,4 +105,5 @@ has the correct title and headers, performanced checks the page, and accessibili
 @pytest.mark.ui
 @pytest.mark.public_companies_products_page
 def test_excess_side_a_bullet_point(page: Page, base_url):
-    
+    # Goes to the home page first
+    page.goto(base_url)
