c36be0f73bd2900bffe9dc5e653dc6d4461adca8
Alex Fergestad <ajfergestad@comcast.net>
2026-06-30 17:00:23 -0500

    Added to public companies products page objects file

--- tests/ui/page_objects/public_companies_products_page_objects.py
+++ tests/ui/page_objects/public_companies_products_page_objects.py
@@ -13,6 +13,7 @@ class Public_Company_Products:
         self.excess_liability_title = page.locator("h3").filter(has_text=re.compile("excess liability", re.IGNORECASE)).first
         self.lead_side_a_title = page.locator("h3").filter(has_text=re.compile(r"lead side[\s-]a", re.IGNORECASE)).first
         self.excess_side_a_title = page.locator("h3").filter(has_text=re.compile(r"excess side[\s-]a", re.IGNORECASE)).first
+        self.contact_us_button = page.get_by_role("link", name="Contact Us")
 
     def navigate_to_products_page(self):
         self.productsPage.click()
