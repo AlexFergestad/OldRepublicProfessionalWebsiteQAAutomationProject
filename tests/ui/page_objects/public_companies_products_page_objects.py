5d1aa01e2dd29a1d19412a73d7719ac4720fdfe6
Alex Fergestad <ajfergestad@comcast.net>
2026-06-30 17:00:45 -0500

    Added to public companies products page objects file

--- tests/ui/page_objects/public_companies_products_page_objects.py
+++ tests/ui/page_objects/public_companies_products_page_objects.py
@@ -61,4 +61,5 @@ class Public_Company_Products:
         expect(self.excess_side_a_links.get_by_role("link", name="Policy form ORUG-92")).to_be_visible()
     
     def verify_contact_us_button(self):
+        expect(self.contact_us_button).to_contain_text("Contact Us")
         expect(self.contact_us_button).to_be_visible()
