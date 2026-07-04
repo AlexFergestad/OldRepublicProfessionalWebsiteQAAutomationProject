a76867595877ec9fedf3268becfd16d701ad563e
Alex Fergestad <ajfergestad@comcast.net>
2026-07-04 16:46:11 -0500

    Added to test public companies products file

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -144,6 +144,7 @@ def test_performance_metrics_products_page(page: Page, base_url):
     # Clicks on the Products link to navigate to the public companies nav bar dropdown
     NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
 
+    # Clicks on the Products link to navigate to the products page
     
 
 # """TC-09: Verify Accessibility Checks"""
