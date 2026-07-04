c0eca80a07afd2be0ad3b41ce41a2e2abbc57a99
Alex Fergestad <ajfergestad@comcast.net>
2026-07-04 16:45:11 -0500

    Added to test public companies products file

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -137,6 +137,6 @@ has the correct title and headers, performanced checks the page, and accessibili
 """TC-08: Verify Performance Metrics"""
 @pytest.mark.ui
 @pytest.mark.public_companies_products_page
-
+def test_performance_metrics_products_page(page: Page, base_url):
 
 # """TC-09: Verify Accessibility Checks"""
