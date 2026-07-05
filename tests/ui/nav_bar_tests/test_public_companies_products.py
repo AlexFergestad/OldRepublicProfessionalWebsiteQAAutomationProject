54550e61d3d3086c7e1304b8261cf82b8d6064cf
Alex Fergestad <ajfergestad@comcast.net>
2026-07-05 13:04:27 -0500

    Added to test public companies products file

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -168,3 +168,5 @@ has the correct title and headers, performanced checks the page, and accessibili
 def test_accessibility_checks_products_page(page: Page, base_url):
     # Goes to the home page first
     page.goto(base_url)
+
+    
