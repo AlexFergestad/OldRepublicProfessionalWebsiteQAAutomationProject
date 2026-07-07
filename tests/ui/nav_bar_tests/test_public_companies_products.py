e65bc6140eb93256a4f228becd0aee7db1977a39
Alex Fergestad <ajfergestad@comcast.net>
2026-07-06 19:56:22 -0500

    Added to test public companies products file

--- tests/ui/nav_bar_tests/test_public_companies_products.py
+++ tests/ui/nav_bar_tests/test_public_companies_products.py
@@ -175,4 +175,4 @@ def test_accessibility_checks_products_page(page: Page, base_url):
     # Clicks on the Products link to navigate to the products page
     Public_Company_Products(page, base_url).navigate_to_products_page()
 
-    
+    # Performs accessibility checks on the products page using Axe
