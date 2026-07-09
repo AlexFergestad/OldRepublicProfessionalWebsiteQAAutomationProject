f2e1d72a8d7e240ceb86fe54bb3ac30a097c7cb8
Alex Fergestad <ajfergestad@comcast.net>
2026-07-08 19:17:31 -0500

    Added to test public companies excess liability file

--- tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
+++ tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
@@ -10,3 +10,12 @@ from tests.ui.page_objects.careers_page_object import CareersPage
 from tests.ui.page_objects.nav_bar_page_objects import NavigationMenu
 from tests.ui.page_objects.public_companies_dando_liability_page_objects import Public_Company_Dando_Liability
 
+"""
+
+Public Companies Excess Liability Page UI Tests
+Test Cases: TC-001, TC-002, 
+
+* This page verifies the Products page of the Old Republic Professional website loads correctly, 
+has the correct title and headers, performanced checks the page, and accessibility checks the page.
+
+"""
