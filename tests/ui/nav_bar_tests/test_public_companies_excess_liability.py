64b843b6fec7cf5fa9a42e79a4f965b8706edaed
Alex Fergestad <ajfergestad@comcast.net>
2026-07-08 19:16:53 -0500

    Added test public companies excess liability file

---
+++ tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
@@ -0,0 +1,12 @@
+# This file is for automating the testing of the excess liability page in the public companies section in the nav bar.
+
+import playwright
+import pytest
+from playwright.sync_api import Page, expect
+from axe_playwright_python.sync_playwright import Axe
+
+# Page Objects - relative import from same ui folder
+from tests.ui.page_objects.careers_page_object import CareersPage
+from tests.ui.page_objects.nav_bar_page_objects import NavigationMenu
+from tests.ui.page_objects.public_companies_dando_liability_page_objects import Public_Company_Dando_Liability
+
