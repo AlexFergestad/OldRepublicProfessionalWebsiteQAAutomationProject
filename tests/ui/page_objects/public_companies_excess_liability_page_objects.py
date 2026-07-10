c34617ebd3d912c4d11fb781f4b0bc414a223d7c
Alex Fergestad <ajfergestad@comcast.net>
2026-07-09 21:33:03 -0500

    Added public companies excess liability page objects file

--- tests/ui/page_objects/public_companies_excess_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_excess_liability_page_objects.py
@@ -4,3 +4,11 @@ from playwright.sync_api import Page, expect
 
 
 class Public_Company_Excess_Liability:
+    def __init__(self, page: Page, base_url: str):
+        self.page = page
+        self.base_url = base_url
+        
+
+    def navigate_to_excess_liability_page(self):
+        self.excess_liability_page.click()
+        self.page.wait_for_load_state("networkidle")
