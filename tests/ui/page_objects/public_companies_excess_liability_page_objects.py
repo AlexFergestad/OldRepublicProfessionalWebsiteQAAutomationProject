f8f16f0044086611a66c1b3bfff77c1c2aa547cd
Alex Fergestad <ajfergestad@comcast.net>
2026-07-15 18:30:23 -0500

    Added to public companies excess liability page objects file

--- tests/ui/page_objects/public_companies_excess_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_excess_liability_page_objects.py
@@ -7,7 +7,7 @@ class Public_Company_Excess_Liability:
     def __init__(self, page: Page, base_url: str):
         self.page = page
         self.base_url = base_url
-        self.
+        self.excess_liability_page = page.locator("#hs_menu_wrapper
         
 
     def navigate_to_excess_liability_page(self):
