7d181ebd29780bd3d11f969e439ef63200e99eac
Alex Fergestad <ajfergestad@comcast.net>
2026-07-16 20:48:45 -0500

    Added to test public companies excess liability.py file

--- tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
+++ tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
@@ -50,4 +50,6 @@ def test_public_companies_excess_liability_page_title(page: Page, base_url):
 
     # Clicks on the Excess Liability link to navigate to the excess liability page
     Public_Company_Excess_Liability(page, base_url).navigate_to_excess_liability_page()
+
+    # Verifies the title of the page is correct
     
