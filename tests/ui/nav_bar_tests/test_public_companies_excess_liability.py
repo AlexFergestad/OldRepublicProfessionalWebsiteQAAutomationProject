146c6b1ba6f51f940a1fafc3fcb3c42c9bb159ff
Alex Fergestad <ajfergestad@comcast.net>
2026-07-19 14:32:47 -0500

    Added to test public companies excess liability.py file

--- tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
+++ tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
@@ -57,3 +57,4 @@ has the correct title and headers, performanced checks the page, and accessibili
 """TC-03: Verify the H1 paragraph underneath has the correct text."""
 @pytest.mark.ui
 @pytest.mark.public_companies_excess_liability
+def test_public_companies_excess_liability_page_title_paragraph(page: Page, base_url):
