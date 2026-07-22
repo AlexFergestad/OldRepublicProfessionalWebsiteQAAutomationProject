08a59fed69cecf0515afb1161a9a4d5cbbe3ae5f
Alex Fergestad <ajfergestad@comcast.net>
2026-07-21 21:40:09 -0500

    Added to test public companies exceess liability

--- tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
+++ tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
@@ -54,18 +54,18 @@ has the correct title and headers, performanced checks the page, and accessibili
 #     # Verifies the title of the page is correct
 #     Public_Company_Excess_Liability(page, base_url).verify_excess_liability_h1()
 
-"""TC-03: Verify the H1 paragraph underneath has the correct text."""
-@pytest.mark.ui
-@pytest.mark.public_companies_excess_liability
-def test_public_companies_excess_liability_page_title_paragraph(page: Page, base_url):
-    # Goes to the home page first
-    page.goto(base_url)
+# """TC-03: Verify the H1 paragraph underneath has the correct text."""
+# @pytest.mark.ui
+# @pytest.mark.public_companies_excess_liability
+# def test_public_companies_excess_liability_page_title_paragraph(page: Page, base_url):
+#     # Goes to the home page first
+#     page.goto(base_url)
 
-    # Clicks on the Public Companies menu item to navigate to the products page
-    NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
+#     # Clicks on the Public Companies menu item to navigate to the products page
+#     NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
 
-    # Clicks on the Excess Liability link to navigate to the excess liability page
-    Public_Company_Excess_Liability(page, base_url).navigate_to_excess_liability_page()
+#     # Clicks on the Excess Liability link to navigate to the excess liability page
+#     Public_Company_Excess_Liability(page, base_url).navigate_to_excess_liability_page()
 
-    Public_Company_Excess_Liability(page, base_url).verify_h1_paragraph()
+#     Public_Company_Excess_Liability(page, base_url).verify_h1_paragraph()
 
