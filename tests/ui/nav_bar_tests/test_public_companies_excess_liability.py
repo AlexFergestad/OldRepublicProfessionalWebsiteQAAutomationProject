1bfe7e36a7e75d64307e8b2000e4d0ebcf0ca029
Alex Fergestad <ajfergestad@comcast.net>
2026-07-27 17:14:28 -0500

    Added to test public companies exceess liability

--- tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
+++ tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
@@ -14,7 +14,7 @@ from tests.ui.page_objects.public_companies_excess_liability_page_objects import
 """
 
 Public Companies Excess Liability Page UI Tests
-Test Cases: TC-001, TC-002, TC-003. TC-004
+Test Cases: TC-001, TC-002, TC-003, TC-004, TC-005
 
 * This page verifies the Excess Liability page of the Old Republic Professional website loads correctly, 
 has the correct title and headers, performanced checks the page, and accessibility checks the page.
@@ -88,5 +88,5 @@ has the correct title and headers, performanced checks the page, and accessibili
 #     Public_Company_Excess_Liability(page, base_url).verify_policy_features()
 
 
-"""TC-05:"""
+"""TC-05: """
     
