8292dbf76b8279790691a931a3e74c9c24317465
Alex Fergestad <ajfergestad@comcast.net>
2026-07-29 20:22:27 -0500

    Added to test_public_companies_excess_liability.py

--- tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
+++ tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
@@ -96,4 +96,5 @@ def test_capacity_attachment_eligibility_and_download_excess_sell_sheet_button(p
     # Goes to the home page first
     page.goto(base_url)
 
-    
+    # Clicks on the Public Companies menu item to navigate to the products page
+    NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
