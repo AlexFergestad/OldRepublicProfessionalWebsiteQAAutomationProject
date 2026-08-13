e56c78dcd1e7c4967c636568646a416a5c9a0c54
Alex Fergestad <ajfergestad@comcast.net>
2026-08-12 20:25:32 -0500

    Added to test public companies excess liability

--- tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
+++ tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
@@ -124,3 +124,4 @@ has the correct title and headers, performanced checks the page, and accessibili
 """TC-07: Verify ."""
 @pytest.mark.ui
 @pytest.mark.public_companies_excess_liability
+def test_our_excess_plus_endorsement_link_and_paragraph_underneath(page: Page, base_url):
