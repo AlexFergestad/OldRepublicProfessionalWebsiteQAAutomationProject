fbd5e690e067d128faebb480dd740949b97db551
Alex Fergestad <ajfergestad@comcast.net>
2026-08-13 21:40:46 -0500

    Added to public compniaes excess liability page objects

--- tests/ui/page_objects/public_companies_excess_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_excess_liability_page_objects.py
@@ -20,7 +20,7 @@ class Public_Company_Excess_Liability:
         self.download_excess_do_sell_sheet = page.get_by_role("link", name="Download Excess D&O Sell Sheet")
         self.our_excess_plus_endorsement = page.get_by_role("link", name="Our Excess Plus™ Endorsement")
         self.our_excess_plus_endorsement_paragraph = page.locator("h3").filter(has_text="Our Excess Plus™ Endorsement").locator("xpath=following-sibling::p[1]")
-        
+        self.how_excess_plus_works = page.locator("h4").filter(has_text="How Excess Plus™ Works")        
 
 
     def navigate_to_excess_liability_page(self):
