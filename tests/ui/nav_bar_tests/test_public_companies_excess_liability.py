2c33e7330fd9d2567ea17f1a254b437ca983734e
Alex Fergestad <ajfergestad@comcast.net>
2026-08-16 18:18:46 -0500

    Added to test public companies excess liability

--- tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
+++ tests/ui/nav_bar_tests/test_public_companies_excess_liability.py
@@ -146,3 +146,5 @@ def test_excess_plus_benefits_and_bullet_points_underneath(page: Page, base_url)
 
     # Clicks on the Public Companies menu item to navigate to the products page
     NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
+
+    
