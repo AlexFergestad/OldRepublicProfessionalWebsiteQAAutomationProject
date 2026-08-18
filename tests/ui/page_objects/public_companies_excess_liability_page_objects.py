d96273ca7e4e9bb7c33b24c7a5e10157ee7436cb
Alex Fergestad <ajfergestad@comcast.net>
2026-08-17 20:30:34 -0500

    Added to excess liability page objects

--- tests/ui/page_objects/public_companies_excess_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_excess_liability_page_objects.py
@@ -81,3 +81,5 @@ class Public_Company_Excess_Liability:
 
     def verify_excess_plus_benefits_and_bullet_points(self):
         expect(self.excess_plus_benefits).to_be_visible(timeout=5000)
+        expect(self.excess_plus_benefits_list).to_be_visible(timeout=5000)
+        self.page.wait_for_load_state("networkidle")
