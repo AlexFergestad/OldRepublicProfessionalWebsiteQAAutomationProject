65470e808866684bce6ac4dab4a2e2520d4e74ad
Alex Fergestad <ajfergestad@comcast.net>
2026-08-20 21:01:48 -0500

    Added to excess liability page objects

--- tests/ui/page_objects/public_companies_excess_liability_page_objects.py
+++ tests/ui/page_objects/public_companies_excess_liability_page_objects.py
@@ -85,6 +85,5 @@ class Public_Company_Excess_Liability:
         self.page.wait_for_load_state("networkidle")
 
     def verify_download_excess_plus_sell_sheet_button(self):
-        expect()
-        expect()
-
+        expect(self.download_excess_plus_sell_sheet).to_be_visible(timeout=5000)
+        expect(self.download_excess_plus_sell_sheet).to_be_enabled(timeout=5000)
