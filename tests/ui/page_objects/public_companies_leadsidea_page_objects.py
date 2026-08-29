0c96300365578b7cf4d8a500c1565fbbd9846398
Alex Fergestad <ajfergestad@comcast.net>
2026-08-29 15:47:10 -0500

    Added to test public companies leadsidea page objects.py

--- tests/ui/page_objects/public_companies_leadsidea_page_objects.py
+++ tests/ui/page_objects/public_companies_leadsidea_page_objects.py
@@ -10,3 +10,5 @@ class Public_Company_Lead_Side_A:
         self.lead_side_a_page = self.header_nav.get_by_role("menuitem", name="Lead Side A")
 
     def navigate_to_lead_side_a_page(self):
+
+        
