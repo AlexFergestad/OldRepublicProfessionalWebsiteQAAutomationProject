3fd34f23619dad4ad14adf025e91bfe74f7fe6bd
Alex Fergestad <ajfergestad@comcast.net>
2026-08-29 15:47:38 -0500

    Added to test public companies leadsidea page objects.py

--- tests/ui/page_objects/public_companies_leadsidea_page_objects.py
+++ tests/ui/page_objects/public_companies_leadsidea_page_objects.py
@@ -7,7 +7,7 @@ class Public_Company_Lead_Side_A:
         self.page = page
         self.base_url = base_url
         self.header_nav = page.locator("#hs_menu_wrapper_module_1527184808535133_mjfm_header_main_menu")
-        self.lead_side_a_page = self.header_nav.get_by_role("menuitem", name="Lead Side A")
+        self.lead_side_a_page = self.header_nav.get_by_role("menuitem", name="Lead Side-A Only")
 
     def navigate_to_lead_side_a_page(self):
         self.lead_side_a_page.click()
