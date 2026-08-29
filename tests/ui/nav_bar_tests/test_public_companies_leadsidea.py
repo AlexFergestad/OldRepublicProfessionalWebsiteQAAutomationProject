2bfd0ffe1c057fff69b1c4974a5f7a457c0b5b2a
Alex Fergestad <ajfergestad@comcast.net>
2026-08-29 15:35:03 -0500

    Added to test public companies leadsidea.py

--- tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
@@ -26,8 +26,10 @@ has the correct title and headers, performanced checks the page, and accessibili
 @pytest.mark.public_companies_lead_side_a_page
 def test_public_companies_lead_side_a_page_loads(page: Page, base_url):
     # Goes to the home page first
-        page.goto(base_url)
+    page.goto(base_url)
 
     # Clicks on the Public Companies menu item to navigate to the products page
-        NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
-        
+    NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
+
+    
+
