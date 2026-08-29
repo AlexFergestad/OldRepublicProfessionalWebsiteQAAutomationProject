49853c64352814f4d0f932835f0d28a0079a5439
Alex Fergestad <ajfergestad@comcast.net>
2026-08-29 15:46:39 -0500

    Added to test public companies leadsidea page objects.py

--- tests/ui/page_objects/public_companies_leadsidea_page_objects.py
+++ tests/ui/page_objects/public_companies_leadsidea_page_objects.py
@@ -1,2 +1,10 @@
+from playwright.async_api import Page
+
+
 class Public_Company_Lead_Side_A:
-    
+
+    def __init__(self, page: Page, base_url: str):
+        self.page = page
+        self.base_url = base_url
+        self.header_nav = page.locator("#hs_menu_wrapper_module_1527184808535133_mjfm_header_main_menu")
+        self.lead_side_a_page = self.header_nav.get_by_role("menuitem", name="Lead Side A")
