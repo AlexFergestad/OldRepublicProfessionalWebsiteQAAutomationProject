6367a9acc920e42562a3943d1f8b390055e82f02
Alex Fergestad <ajfergestad@comcast.net>
2026-09-03 19:14:46 -0500

    Added to test public companies lead side a

--- tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
@@ -68,3 +68,8 @@ def test_public_companies_lead_side_a_h1_paragraph(page: Page, base_url):
 
     # Verifies that the paragraph underneath 'Lead Side-A D&O: "A-Sure' is visible
     Public_Company_Lead_Side_A(page, base_url).verify_lead_side_a_h1_paragraph()
+
+
+
+
+    
