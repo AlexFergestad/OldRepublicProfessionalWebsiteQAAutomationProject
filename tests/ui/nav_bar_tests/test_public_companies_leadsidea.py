dc72de17055a4603096f10ca4ef8c663638dbe7d
Alex Fergestad <ajfergestad@comcast.net>
2026-09-04 19:26:08 -0500

    Added to test public companies lead side a

--- tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
@@ -74,3 +74,5 @@ def test_public_companies_lead_side_a_h1_paragraph(page: Page, base_url):
 @pytest.mark.ui
 @pytest.mark.public_companies_lead_side_a_page
 def test_public_companies_lead_side_a_download_buttons(page: Page, base_url):
+
+
