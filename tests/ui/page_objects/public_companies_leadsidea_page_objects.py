e65842291d565a87c4766507cf7be8c37f58e6b3
Alex Fergestad <ajfergestad@comcast.net>
2026-09-07 21:52:27 -0500

    Added to test public companies leadsidea page objects.py

--- tests/ui/page_objects/public_companies_leadsidea_page_objects.py
+++ tests/ui/page_objects/public_companies_leadsidea_page_objects.py
@@ -17,4 +17,4 @@ class Public_Company_Lead_Side_A:
     def verify_lead_side_a_page_title_and_headers(self):
         expect(self.lead_side_a_page_title).to_be_visible(timeout=5000)
 
-    def 
+    def verify_download_buttons(self):
