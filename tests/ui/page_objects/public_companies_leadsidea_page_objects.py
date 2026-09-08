dd4a2e74f258e3baaaffa4a873c2670ddaa23c76
Alex Fergestad <ajfergestad@comcast.net>
2026-09-07 21:56:53 -0500

    Added to test public companies leadsidea page objects.py

--- tests/ui/page_objects/public_companies_leadsidea_page_objects.py
+++ tests/ui/page_objects/public_companies_leadsidea_page_objects.py
@@ -28,7 +28,7 @@ class Public_Company_Lead_Side_A:
 
         expect(self.download_policy_forms).to_be_visible(timeout=5000)
         self.download_policy_forms.click()
-        
+        self.page.go_back()
         self.page.wait_for_load_state("networkidle")
 
 
