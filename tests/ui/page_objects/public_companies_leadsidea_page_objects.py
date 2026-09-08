7fe3e2b025576b00351435e8c3d2ea89cae592b5
Alex Fergestad <ajfergestad@comcast.net>
2026-09-07 21:53:56 -0500

    Added to test public companies leadsidea page objects.py

--- tests/ui/page_objects/public_companies_leadsidea_page_objects.py
+++ tests/ui/page_objects/public_companies_leadsidea_page_objects.py
@@ -9,7 +9,7 @@ class Public_Company_Lead_Side_A:
         self.header_nav = page.locator("#hs_menu_wrapper_module_1527184808535133_mjfm_header_main_menu")
         self.lead_side_a_page = self.header_nav.get_by_role("menuitem", name="Lead Side-A Only")
         self.lead_side_a_page_title = page.locator("h1").filter(has_text="A-Sure")
-        self.download_lead = page.locator("a:has-text(\"Download Lead Side-A\")")
+        self.download_lead = page.locator("a:has-text(\"Download Lead Side-A-Sure\" Sell Sheet\")")
 
     def navigate_to_lead_side_a_page(self):
         self.lead_side_a_page.click()
