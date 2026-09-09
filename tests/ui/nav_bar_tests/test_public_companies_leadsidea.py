d8896ece7babd6bfbe957d3cccc7a73798c2b0e8
Alex Fergestad <ajfergestad@comcast.net>
2026-09-09 18:08:51 -0500

    Addes to test public companies lead side a

--- tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
@@ -90,3 +90,5 @@ has the correct title and headers, performanced checks the page, and accessibili
 @pytest.mark.ui
 @pytest.mark.public_companies_lead_side_a_page
 def test_public_companies_lead_side_a_page_performance(page: Page, base_url):
+
+    
