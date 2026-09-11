96cca1197f1cf52ca8c5b47489d0ce7b7d79d98a
Alex Fergestad <ajfergestad@comcast.net>
2026-09-11 11:26:12 -0500

    Addes to test public companies lead side a

--- tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
@@ -122,4 +122,4 @@ def test_public_companies_lead_side_a_page_accessibility(page: Page, base_url):
     # Clicks on the Public Companies menu item to navigate to the products page
     NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
 
-    
+    # Clicks on the Lead Side-A link to navigate to the lead side a page
