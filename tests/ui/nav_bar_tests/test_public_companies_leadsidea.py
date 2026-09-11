3fb0bddc33219625ac4ab3f8c45581a19e1af237
Alex Fergestad <ajfergestad@comcast.net>
2026-09-11 11:26:36 -0500

    Addes to test public companies lead side a

--- tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_leadsidea.py
@@ -125,4 +125,4 @@ def test_public_companies_lead_side_a_page_accessibility(page: Page, base_url):
     # Clicks on the Lead Side-A link to navigate to the lead side a page
     Public_Company_Lead_Side_A(page, base_url).navigate_to_lead_side_a_page()
 
-    
+    # Verifies the Accessibility of the public companies lead side a page using axe-playwright-python
