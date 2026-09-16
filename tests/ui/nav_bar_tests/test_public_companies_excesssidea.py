cb32d6136d74462bb733654e14c1f2b34ce1c237
Alex Fergestad <ajfergestad@comcast.net>
2026-09-16 07:27:49 -0500

    Added to test public companies excess side a

--- tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
@@ -7,3 +7,13 @@ from axe_playwright_python.sync_playwright import Axe
 
 # Page Objects - relative import from same ui folder
 
+
+"""
+
+Public Companies Excess Side A Page UI Tests
+Test Cases: TC-001, TC-002,
+
+* This page verifies the Excess Side A page of the Old Republic Professional website loads correctly, 
+has the correct title and headers, performanced checks the page, and accessibility checks the page.
+
+"""
