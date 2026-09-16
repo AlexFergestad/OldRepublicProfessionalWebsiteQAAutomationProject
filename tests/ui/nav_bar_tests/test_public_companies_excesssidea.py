139acc96f0ebd0ffd6cff9a50b847a6c0852c9c6
Alex Fergestad <ajfergestad@comcast.net>
2026-09-16 07:28:16 -0500

    Added to test public companies excess side a

--- tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
@@ -19,4 +19,4 @@ has the correct title and headers, performanced checks the page, and accessibili
 """
 
 
-
+"""TC-01: Verify that the public companies excess side a page loads correctly and has the correct URL when accessed from the home page."""
