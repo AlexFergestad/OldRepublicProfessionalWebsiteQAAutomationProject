0e7d3ddcafb416707e781eeea7537dea61775a1c
Alex Fergestad <ajfergestad@comcast.net>
2026-09-16 17:20:19 -0500

    Added to test public companies excess side a

--- tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
@@ -31,3 +31,5 @@ def test_public_companies_excess_side_a_page_loads(page: Page, base_url):
 
     # Clicks on the Public Companies menu item to navigate to the exccess side a page
     NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
+
+    
