11e120f4b0d23fe5a7c6b9f386d242c8b2fa0c24
Alex Fergestad <ajfergestad@comcast.net>
2026-09-16 07:27:08 -0500

    Added to test public companies excess side a

---
+++ tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
@@ -0,0 +1,6 @@
+# This file is for automating the testing of the excess side a page in the public companies section in the nav bar.
+
+import playwright
+import pytest
+from playwright.sync_api import Page, expect
+from axe_playwright_python.sync_playwright import Axe
