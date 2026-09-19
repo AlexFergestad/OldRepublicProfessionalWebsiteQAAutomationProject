9705b6238574825cb8915478ef9015a9fe879765
Alex Fergestad <ajfergestad@comcast.net>
2026-09-18 22:40:12 -0500

    Added to public companiies excess side a  page objects

---
+++ tests/ui/page_objects/public_companies_excess_sidea_page_objects.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Page, expect
+
+
+class Public_Company_Lead_Side_A:
+
+    def __init__(self, page: Page, base_url: str):
+        self.page = page
+        self.base_url = base_url
+        self.header_nav = page.locator("#hs_menu_wrapper_module_1527184808535133_mjfm_header_main_menu")
