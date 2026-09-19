9bf4d7b5a436ea0c786c75762c8bef3bbcde4c47
Alex Fergestad <ajfergestad@comcast.net>
2026-09-19 17:05:44 -0500

    Added to test public companies excess side a

--- tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
@@ -37,6 +37,5 @@ def test_public_companies_excess_side_a_page_loads(page: Page, base_url):
     Public_Company_Excess_Side_A(page, base_url).navigate_to_excess_side_a_page()
 
     # Verifies that the page has loaded correctly by checking the URL and the page title
-    
+    page.wait_for_load_state("networkidle")
 
-    
