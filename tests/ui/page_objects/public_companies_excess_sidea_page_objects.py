9e4dffd5dc00ff3c4564adbc0a39f005b88ad4f3
Alex Fergestad <ajfergestad@comcast.net>
2026-09-18 22:40:33 -0500

    Added to public companiies excess side a page objects

--- tests/ui/page_objects/public_companies_excess_sidea_page_objects.py
+++ tests/ui/page_objects/public_companies_excess_sidea_page_objects.py
@@ -7,3 +7,6 @@ class Public_Company_Lead_Side_A:
         self.page = page
         self.base_url = base_url
         self.header_nav = page.locator("#hs_menu_wrapper_module_1527184808535133_mjfm_header_main_menu")
+
+
+        
