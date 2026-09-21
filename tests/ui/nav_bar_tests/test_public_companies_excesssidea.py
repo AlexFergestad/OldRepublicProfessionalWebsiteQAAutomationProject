b946eb8fd61bca9120636b5f6f9fe68763145cfd
Alex Fergestad <ajfergestad@comcast.net>
2026-09-20 20:11:34 -0500

    Added to test public companies excess side a

--- tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
@@ -39,7 +39,7 @@ def test_public_companies_excess_side_a_page_loads(page: Page, base_url):
     # Verifies that the page has loaded correctly by checking the URL and the page title
     page.wait_for_load_state("networkidle")
 
-"""TC-002:"""
+"""TC-002: Verify that the public companies excess side a page has the correct title and headers."""
 
 
 
