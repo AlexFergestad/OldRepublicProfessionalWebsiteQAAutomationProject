8bc4859e065098a303662855d9576004741cdac6
Alex Fergestad <ajfergestad@comcast.net>
2026-09-21 17:13:33 -0500

    Added to public companiies excess side a page objects

--- tests/ui/page_objects/public_companies_excess_sidea_page_objects.py
+++ tests/ui/page_objects/public_companies_excess_sidea_page_objects.py
@@ -9,6 +9,8 @@ class Public_Company_Excess_Side_A:
         self.header_nav = page.locator("#hs_menu_wrapper_module_1527184808535133_mjfm_header_main_menu")
 
         self.excess_side_a_page = self.header_nav.get_by_role("menuitem", name="Excess Side-A Only")
+        self.title = page.locator("h1")
+        self.
 
     def navigate_to_excess_side_a_page(self):
         self.page.wait_for_timeout(1000)
@@ -16,6 +18,9 @@ class Public_Company_Excess_Side_A:
         self.excess_side_a_page.click()
         self.page.wait_for_load_state("networkidle")
 
-    def verify_page_title_and_headers(self):
-
+    def verify_page_title_and_header(self):
+        # Verifies that the page has the correct title and header
+        expect(self.page).to_have_title("Excess Side-A Only | Old Republic Professional")
+        expect(self.page.locator("h1")).to_have_text("Excess Side-A Only")
+        expect(self.page.locator("h2")).to_have_text("Excess Side-A Only")
 
