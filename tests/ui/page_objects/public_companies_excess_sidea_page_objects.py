1028edd9bf79f07fcc767d2afcfb3cdfd13cabad
Alex Fergestad <ajfergestad@comcast.net>
2026-09-21 17:12:44 -0500

    Added to public companiies excess side a page objects

--- tests/ui/page_objects/public_companies_excess_sidea_page_objects.py
+++ tests/ui/page_objects/public_companies_excess_sidea_page_objects.py
@@ -16,6 +16,6 @@ class Public_Company_Excess_Side_A:
         self.excess_side_a_page.click()
         self.page.wait_for_load_state("networkidle")
 
-    def 
+    def verify_page_title_and_headers(self):
 
 
