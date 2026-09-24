5befbcf71106fcdf90b6bc077101122263636a05
Alex Fergestad <ajfergestad@comcast.net>
2026-09-24 17:24:30 -0500

    Added to public companiies excess side a page objects

--- tests/ui/page_objects/public_companies_excess_sidea_page_objects.py
+++ tests/ui/page_objects/public_companies_excess_sidea_page_objects.py
@@ -10,6 +10,7 @@ class Public_Company_Excess_Side_A:
 
         self.excess_side_a_page = self.header_nav.get_by_role("menuitem", name="Excess Side-A Only")
         self.title = page.locator("h1")
+        self.policy_features = page.locator("h2:has-text('Policy Features (ORUG-92):')")
 
     def navigate_to_excess_side_a_page(self):
         self.page.wait_for_timeout(1000)
@@ -24,7 +25,7 @@ class Public_Company_Excess_Side_A:
 
     def verify_policy_features_bullet_points(self):
         # Verifies that the page has the correct policy features bullet point list, capacity, and eligibility text
-        expect(self.page.locator("h2")).to_contain_text("Policy Features (ORUG-92):")
+        expect(self.policy_features).to_contain_text("Policy Features (ORUG-92):")
         expect(self.page.locator("ul li")).to_have_count(5)
         expect(self.page.locator("ul li").nth(0)).to_contain_text("One-page streamlined")
         expect(self.page.locator("ul li").nth(1)).to_contain_text("Independent Director Liability")
