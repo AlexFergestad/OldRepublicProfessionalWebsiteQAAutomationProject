27c3639358fe95c8acd7b8e8381c1cf139bccb43
Alex Fergestad <ajfergestad@comcast.net>
2026-09-24 17:13:59 -0500

    Added to test public companies excess side a

--- tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
+++ tests/ui/nav_bar_tests/test_public_companies_excesssidea.py
@@ -67,5 +67,6 @@ def test_public_companies_excess_side_a_page_policy_features(page: Page, base_ur
     NavigationMenu(page).navigate_to_nav_bar_item("Public Companies")
 
     # Clicks on the Excess Side A link to navigate to the excess liability page
+    Public_Company_Excess_Side_A(page, base_url).navigate_to_excess_side_a_page()
 
 
